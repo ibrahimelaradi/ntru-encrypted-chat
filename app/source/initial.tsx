319fbc1b73b6dded54d815e6f86bcc58fb5485a1
Darkystel <ibrahimelaradi@gmail.com>
2020-12-18 17:19:58 +0200

    fixed raw mode issue on windows

--- app/source/initial.tsx
+++ app/source/initial.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Box, Text } from "ink";
+import React, { useEffect } from "react";
+import { Box, Text, useStdin } from "ink";
 import Spinner from "ink-spinner";
 import { useAppContext } from "./context/ntru";
 import UI from "./ui";
@@ -7,6 +7,14 @@ import { Provider as SocketProvider } from "./socket";
 
 export default function Initail() {
 	const { initializing } = useAppContext();
+	const { isRawModeSupported, setRawMode } = useStdin();
+
+	useEffect(() => {
+		console.log(isRawModeSupported);
+		if (!isRawModeSupported) setRawMode(false);
+		else setRawMode(true);
+	}, []);
+
 	return initializing ? (
 		<Box flexGrow={1} borderStyle="round" flexDirection="row">
 			<Spinner type="arc" />
